4f5a4cdb1088bcd55590c44e275332d76328b71a
Isaac <isaactian.contact@gmail.com>
2024-03-16 19:18:27 -0400

    fire scene

---
+++ QueerControllerUni/Assets/Scripts/EnableFIre.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableFIre : MonoBehaviour
+{
+    public ParticleSystem theFire;
+
+    public bool isLit;
+
+    void Start()
+    {
+        isLit = false;
+    }
+
+    void Update()
+    {
+        if (isLit == false)
+        {
+            theFire.Pause();
+        }
+
+        if (isLit == true)
+        {
+            theFire.GetComponentInChildren<Light>().intensity = 1f;  
+            theFire.Play();
+        }
+    }
+}
