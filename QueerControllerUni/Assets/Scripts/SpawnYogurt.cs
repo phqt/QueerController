a852dd4b3f34a688c4b5af049a4fdc36fe8c14a3
Isaac <isaactian.contact@gmail.com>
2024-03-26 23:22:01 -0400

    arduino

--- QueerControllerUni/Assets/Scripts/SpawnYogurt.cs
+++ QueerControllerUni/Assets/Scripts/SpawnYogurt.cs
@@ -5,41 +5,79 @@ using UnityEngine.SceneManagement;
 
 public class SpawnYogurt : MonoBehaviour
 {
-    public GameObject[] objectsToActivate; 
-    private int currentIndex = 0;
+    //public GameObject[] objectsToActivate; 
+    //private int currentIndex = 0;
     public float sceneSwitchDelay;
+
+    public GameObject yogurtOne;
+    public GameObject yogurtTwo;
+    public GameObject yogurtThree;
+    public GameObject yogurtFour;
+
+    public bool firstYogurt;
+    public bool secondYogurt;
+    public bool thirdYogurt;
+    public bool fourthYogurt;
+
+    //public GameObject yogurdArdHolder;
+    //public yogurtArd yogurtScript;
 
     void Update()
     {
 
-        if (Input.GetKeyDown(KeyCode.E))
+        //if (Input.GetKeyDown(KeyCode.E))
+        //{
+        //    ActivateNextObject();
+        //}
+
+        if (firstYogurt == true)
         {
-            ActivateNextObject();
+            yogurtOne.SetActive(true);
+        }
+
+        if (secondYogurt == true)
+        {
+            yogurtTwo.SetActive(true);
+        }
+
+        if (thirdYogurt == true)
+        {
+            yogurtThree.SetActive(true);
+        }
+
+        if (fourthYogurt == true)
+        {
+            yogurtFour.SetActive(true);
+        }
+
+        if (firstYogurt == true & secondYogurt == true & thirdYogurt ==true & fourthYogurt == true)
+        {
+            StartCoroutine(nextScene());
         }
     }
 
-    void ActivateNextObject()
-    {
+    //void ActivateNextObject()
+    //{
 
-        if (objectsToActivate != null && objectsToActivate.Length > 0)
-        {
+    //    if (objectsToActivate != null && objectsToActivate.Length > 0)
+    //    {
 
-            if (currentIndex < objectsToActivate.Length)
-            {
-                objectsToActivate[currentIndex].SetActive(true);
-                currentIndex++;
-            }
-            else
-            {
-                Debug.LogWarning("All objects in the array have been activated!");
-                StartCoroutine(nextScene());
-            }
-        }
-        else
-        {
-            Debug.LogError("objectsToActivate array is not assigned or empty!");
-        }
-    }
+    //        if (currentIndex < objectsToActivate.Length)
+    //        {
+    //            objectsToActivate[currentIndex].SetActive(true);
+    //            currentIndex++;
+    //        }
+    //        else
+    //        {
+    //            Debug.LogWarning("All objects in the array have been activated!");
+    //            StartCoroutine(nextScene());
+    //        }
+    //    }
+    //    else
+    //    {
+    //        Debug.LogError("objectsToActivate array is not assigned or empty!");
+    //    }
+    //}
 
     IEnumerator nextScene()
     {
---
+++ QueerControllerUni/Assets/Scripts/yogurtArd.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class yogurtArd : MonoBehaviour
+{
+    public SerialController serialController;
+    public SpawnYogurt spawnYogurt;
+    public GameObject yogurtOne;
+
+    void OnMessageArrived(string stat)
+    {
+        Debug.Log(stat);
+
+        if (stat == "pressureSensor1")
+        {
+            Debug.Log("one");
+            //spawnYogurt.firstYogurt = true;
+            yogurtOne.SetActive(true);
+        }
+        if (stat == "2")
+        {
+            Debug.Log("two");
+        }
+        if (stat == "3")
+        {
+            Debug.Log("three");
+        }
+        if (stat == "4")
+        {
+            Debug.Log("four");
+        }
+    }
+
+    void OnConnectionEvent(bool success)
+    {
+        Debug.Log(success ? "Device connected" : "Device disconnected");
+    }
+}
